e5655c9d94025dc951d90f5b682c50e5c5e503ea
rushba1911 <121183841+rushba1911@users.noreply.github.com>
2022-12-24 03:31:41 -0800

    Add files via upload

---
+++ sum3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int num1;
+int num2;
+cout<<"Enter first number ";
+cin>>num1;
+cout<<"Enter second number ";
+cin>>num2;
+num1=num1+num2;
+cout<<"Enter third number ";
+cin>>num2;
+num1=num1+num2;
+cout<<"Enter fourth number ";
+cin>>num2;
+num1=num1+num2;
+cout<<"Enter fifth number ";
+cin>>num2;
+num1=num1+num2;
+cout<<"Sum is "<<num1;
+}
